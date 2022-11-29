75d09f2c0c91857afbc145c34ee02fc66d14d457
Mini <minnnidev@gmail.com>
2022-11-29 22:48:31 +0900

    [Feat] SearchBar 구현 #4

--- Kurly/Kurly/Presentation/Search/SearchViewController.swift
+++ Kurly/Kurly/Presentation/Search/SearchViewController.swift
@@ -7,23 +7,72 @@
 
 import UIKit
 
+import SnapKit
+import Then
+
+// MARK: - SearchViewController
+
 class SearchViewController: UIViewController {
+    
+    // MARK: - UI Components
+    
+    private lazy var searchBar = UISearchBar().then {
+        $0.delegate = self
+    }
+
+    
+    // MARK: - Life Cycles
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        setNavigationBar()
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        layout()
+    }
+}
 
-        // Do any additional setup after loading the view.
+// MARK: - Extension
+
+extension SearchViewController {
+    
+    // MARK: - Layout Helper
+    
+    private func layout() {
+        
+        view.addSubviews(searchBar)
+        
+        searchBar.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide)
+            $0.leading.trailing.equalToSuperview()
+            $0.height.equalTo(60)
+        }
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // MARK: - General Helper
+    
+    private func setNavigationBar() {
+        self.navigationController?.navigationBar.isHidden = false
+        self.navigationController?.navigationBar.backgroundColor = .kurlyPurple
+        
+        self.navigationItem.title = "검색"
+        self.navigationController?.navigationBar.titleTextAttributes = [
+            NSAttributedString.Key.foregroundColor: UIColor.white,
+            NSAttributedString.Key.font: UIFont.systemFont(ofSize: 17, weight: .medium)
+        ]
     }
-    */
+}
 
+// MARK: - UISearchBarDelegate
+
+extension SearchViewController: UISearchBarDelegate {
+    
+    func searchBarTextDidBeginEditing(_ searchBar: UISearchBar) {
+        print("검색 창으로 이동!")
+    }
 }
