7813ef8b3105b61b38e45450b726ffdd77fc7540
Mini <minnnidev@gmail.com>
2022-11-29 15:47:12 +0900

    [Setting] 프로젝트 생성 #1

---
+++ Kurly/Kurly/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Kurly
+//
+//  Created by 김민 on 2022/11/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
