ac0d8388f0fb5d6d810428abd77eaa473164093b
Mini <minnnidev@gmail.com>
2022-12-18 16:50:23 +0900

    [Refactor] Compositional Layout - 최근 검색어 구현 #8

--- Kurly/Kurly/Presentation/Search/SearchViewController.swift
+++ Kurly/Kurly/Presentation/Search/SearchViewController.swift
@@ -14,20 +14,17 @@ import Then
 
 class SearchViewController: UIViewController {
     
-    // MARK: - UI Components
+    // MARK: - Enum
     
-    private lazy var tableView: UITableView = {
-        let tableView = UITableView(frame: .zero, style: .grouped)
-        
-        tableView.backgroundColor = .white
-        tableView.translatesAutoresizingMaskIntoConstraints = false
-        tableView.separatorStyle = .none
-        tableView.delegate = self
-        tableView.dataSource = self
-        tableView.showsVerticalScrollIndicator = false
-        
-        return tableView
-    }()
+    enum Section {
+        case recent
+    }
+    
+    // MARK: - Variable
+    
+    var dataSource: UICollectionViewDiffableDataSource<Section, String>! = nil
+    
+    // MARK: - UI Components
     
     private let searchView = UIView().then {
         $0.backgroundColor = .white
@@ -39,6 +36,16 @@ class SearchViewController: UIViewController {
         $0.layer.cornerRadius = 5
         $0.delegate = self
     }
+    
+    private lazy var searchCollectionView: UICollectionView = {
+        let layout = createCompositonalLayout()
+        
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.showsVerticalScrollIndicator = false
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        
+        return collectionView
+    }()
 
     
     // MARK: - Life Cycles
@@ -52,8 +59,7 @@ class SearchViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        layout()
-        register()
+        setLayout()
         config()
     }
 }
@@ -64,10 +70,38 @@ extension SearchViewController {
     
     // MARK: - Layout Helper
     
-    private func layout() {
+    private func createCompositonalLayout() -> UICollectionViewCompositionalLayout {
+        return UICollectionViewCompositionalLayout { (sectionInt, layoutEnvironment) -> NSCollectionLayoutSection in
+            switch sectionInt {
+            case 0:
+                return self.recentLayoutSection()
+            default:
+                return self.recentLayoutSection()
+            }
+        }
+    }
+    
+    private func recentLayoutSection() -> NSCollectionLayoutSection {
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
+                                              heightDimension: .fractionalHeight(1.0))
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        item.contentInsets =  NSDirectionalEdgeInsets(top: 0.0, leading: 5, bottom: 0.0, trailing: 5)
+        
+        let groupSize = NSCollectionLayoutSize(widthDimension: .estimated(80),
+                                               heightDimension: .absolute(35))
+        let group = NSCollectionLayoutGroup.vertical(layoutSize: groupSize, subitems: [item])
+
+        let section = NSCollectionLayoutSection(group: group)
+        
+        section.orthogonalScrollingBehavior = .continuous
+        
+        return section
+    }
+    
+    private func setLayout() {
         
         view.backgroundColor = .kurlyPurple
-        view.addSubviews(searchView,tableView)
+        view.addSubviews(searchView, searchCollectionView)
         searchView.addSubview(searchTextField)
         
         searchView.snp.makeConstraints {
@@ -82,7 +116,7 @@ extension SearchViewController {
             $0.centerX.centerY.equalToSuperview()
         }
         
-        tableView.snp.makeConstraints {
+        searchCollectionView.snp.makeConstraints {
             $0.top.equalTo(searchView.snp.bottom)
             $0.leading.trailing.equalTo(view.safeAreaLayoutGuide)
             $0.bottom.equalToSuperview()
@@ -93,6 +127,19 @@ extension SearchViewController {
     
     private func config() {
         searchTextField.setIcon(UIImage(systemName: "magnifyingglass")!)
+        
+        let recentCellRegisteration = UICollectionView.CellRegistration<RecentCollectionViewCell, String> { cell, indexPath, itemIdentifier in
+            cell.dataBind(productName: itemIdentifier)
+        }
+        
+        dataSource = UICollectionViewDiffableDataSource<Section, String> (collectionView: searchCollectionView, cellProvider: { collectionView, indexPath, itemIdentifier in
+            return collectionView.dequeueConfiguredReusableCell(using: recentCellRegisteration, for: indexPath, item: itemIdentifier)
+        })
+        
+        var snapshot = NSDiffableDataSourceSnapshot<Section, String>()
+        snapshot.appendSections([.recent])
+        snapshot.appendItems(recentData)
+        dataSource.apply(snapshot, animatingDifferences: false)
     }
     
     private func setNavigationBar() {
@@ -104,12 +151,6 @@ extension SearchViewController {
             NSAttributedString.Key.foregroundColor: UIColor.white,
             NSAttributedString.Key.font: UIFont.systemFont(ofSize: 17, weight: .medium)
         ]
-    }
-    
-    private func register() { 
-
-        tableView.register(RecommendSearchTableViewCell.self, forCellReuseIdentifier: RecommendSearchTableViewCell.identifier)
-        tableView.register(RiseSearchTableViewCell.self, forCellReuseIdentifier: RiseSearchTableViewCell.identifier)
     }
 }
 
@@ -124,49 +165,21 @@ extension SearchViewController: UITextFieldDelegate {
 
 // MARK: - UITableViewDataSource
 
-extension SearchViewController: UITableViewDataSource {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 2
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        switch indexPath.row {
-        case 0:
-            guard let cell = tableView.dequeueReusableCell(withIdentifier: RecommendSearchTableViewCell.identifier) as? RecommendSearchTableViewCell else {return UITableViewCell()}
-            
-            return cell
-        case 1:
-            guard let cell = tableView.dequeueReusableCell(withIdentifier: RiseSearchTableViewCell.identifier) as? RiseSearchTableViewCell else {return UITableViewCell()}
-            
-            return cell
-        default:
-            return UITableViewCell()
-        }
-    }
-    
-    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
-        let view = SearchTableViewHeader()
-        return view
-    }
-    
-    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
-        return 100
-    }
-}
+
 
 // MARK: - UITableViewDelegate
 
-extension SearchViewController: UITableViewDelegate {
-    
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        
-        switch indexPath.row {
-        case 0:
-            return 150
-        case 1:
-            return 570
-        default:
-            return 0
-        }
-    }
-}
+//extension SearchViewController: UITableViewDelegate {
+//    
+//    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+//        
+//        switch indexPath.row {
+//        case 0:
+//            return 150
+//        case 1:
+//            return 570
+//        default:
+//            return 0
+//        }
+//    }
+//}
